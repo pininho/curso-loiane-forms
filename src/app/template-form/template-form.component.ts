e429e000ad8acaf221ee0d72e60d2c48deddd3be
Alessandro Pino <alessandro@profissionalti.com>
2022-02-21 15:01:01 -0300

    uso do setValue e patchValue

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -30,7 +30,7 @@ export class TemplateFormComponent implements OnInit {
     return {'is-invalid': !campo.valid && campo.touched};
   }
 
-  consultaCEP(cep) {
+  consultaCEP(cep, form) {
     //console.log(cep);
     //nova var cep só com dígitos
     cep = cep.replace(/\D/g, '');
@@ -40,11 +40,50 @@ export class TemplateFormComponent implements OnInit {
       var validacep = /^[0-9]{8}$/;
       //validar o formato do cep
       if(validacep.test(cep)) {
+        this.resetaDadosForm(form);
         this.http.get(`//viacep.com.br/ws/${cep}/json/`)
         .pipe(map(dados =>  dados))
-        .subscribe(dados => console.log(dados))
+        .subscribe(dados => this.populaDadosForm(dados, form));
       }
     }
+  }
+
+  populaDadosForm(dados, form) {
+    /*form.setValue({
+      nome: form.value.nome,
+      email: form.value.email,
+      endereco: {
+        cep: dados.cep,
+        rua: dados.logradouro,
+        numero:'',
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });*/
+
+    form.form.patchValue({
+      endereco: {
+        rua: dados.logradouro,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });
+  }
+
+  resetaDadosForm(form) {
+    form.form.patchValue({
+      endereco: {
+        rua: null,
+        complemento: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    });
   }
 
 }
