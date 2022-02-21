0767276530bf937f537eb471b08846b43d233318
Alessandro Pino <alessandro@profissionalti.com>
2022-02-21 14:08:18 -0300

    utilizando ws para obter os dados automaticamente do cep

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-template-form',
@@ -19,13 +21,30 @@ export class TemplateFormComponent implements OnInit {
     console.log(this.usuario);
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
   aplicaCssErro(campo) {
     return {'is-invalid': !campo.valid && campo.touched};
+  }
+
+  consultaCEP(cep) {
+    //console.log(cep);
+    //nova var cep só com dígitos
+    cep = cep.replace(/\D/g, '');
+    //verifica se cep tem valor
+    if(cep != "") {
+      //expressão regular para validar o cep
+      var validacep = /^[0-9]{8}$/;
+      //validar o formato do cep
+      if(validacep.test(cep)) {
+        this.http.get(`//viacep.com.br/ws/${cep}/json/`)
+        .pipe(map(dados =>  dados))
+        .subscribe(dados => console.log(dados))
+      }
+    }
   }
 
 }
