9469e3af9eebf4ca38ea218568968fe9fc728a80
Alessandro Pino <alessandro@profissionalti.com>
2022-02-21 16:44:54 -0300

    submetendo form com http post

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -17,8 +17,10 @@ export class TemplateFormComponent implements OnInit {
 
   onSubmit(form) {
     console.log(form.value);
+    //console.log(this.usuario);
 
-    console.log(this.usuario);
+    this.http.post('https://httpbin.org/post', JSON.stringify(form.value))
+      .pipe(map(res => res)).subscribe(dados => console.log(dados));
   }
 
   constructor(private http: HttpClient) { }
