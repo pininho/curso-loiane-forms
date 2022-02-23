c3d9d2b6011b8125ae2ff36084b48d34d55abe07
Alessandro Pino <alessandro@profissionalti.com>
2022-02-23 15:13:24 -0300

    configurado setValue e patchValue

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -73,4 +73,64 @@ export class DataFormComponent implements OnInit {
     }
   }
 
+  consultaCEP() {
+
+    let cep = this.formulario.get('endereco.cep').value;
+
+    //console.log(cep);
+    //nova var cep só com dígitos
+    cep = cep.replace(/\D/g, '');
+    //verifica se cep tem valor
+    if(cep != "") {
+      //expressão regular para validar o cep
+      var validacep = /^[0-9]{8}$/;
+      //validar o formato do cep
+      if(validacep.test(cep)) {
+        this.resetaDadosForm();
+        this.http.get(`//viacep.com.br/ws/${cep}/json/`)
+        .pipe(map(dados =>  dados))
+        .subscribe(dados => this.populaDadosForm(dados));
+      }
+    }
+  }
+
+  resetaDadosForm() {
+    this.formulario.patchValue({
+      endereco: {
+        rua: null,
+        complemento: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    });
+  }
+
+  populaDadosForm(dados) {
+    /*form.setValue({
+      nome: form.value.nome,
+      email: form.value.email,
+      endereco: {
+        cep: dados.cep,
+        rua: dados.logradouro,
+        numero:'',
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });*/
+
+    this.formulario.patchValue({
+      endereco: {
+        rua: dados.logradouro,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });
+
+  }
+
 }
